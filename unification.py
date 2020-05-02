e7e0661162f18129356b41bb3b040c94b8e04eb4
Puneeth K <32433964+punndcoder28@users.noreply.github.com>
2020-05-02 22:32:54 +0530

    Update unification.py

--- unification.py
+++ unification.py
@@ -1,120 +1,94 @@
-def unify(x, y, s):
-    """Unify expressions x,y with substitution s; return a substitution that
-    would make x,y equal, or None if x,y can not unify. x and y can be
-    variables (e.g. Expr('x')), constants, lists, or Exprs. [Fig. 9.1]
-    >>> ppsubst(unify(x + y, y + C, {}))
-    {x: y, y: C}
-    """
-    if s is None:
-        return None
-    elif x == y:
-        return s
-    elif is_variable(x):
-        return unify_var(x, y, s)
-    elif is_variable(y):
-        return unify_var(y, x, s)
-    elif isinstance(x, Expr) and isinstance(y, Expr):
-        return unify(x.args, y.args, unify(x.op, y.op, s))
-    elif isinstance(x, str) or isinstance(y, str):
-        return None
-    elif issequence(x) and issequence(y) and len(x) == len(y):
-        if not x: return s
-        return unify(x[1:], y[1:], unify(x[0], y[0], s))
-    else:
-        return None
+no_of_pred=0
+no_of_arg=[None for i in range(10)]
+nouse=''
+predicate=[None for i in range(10)]
+argument=[[None for i in range(10)] for i in range(10)]
+ 
+def main():
+    global no_of_pred
+    ch='y'
+    while(ch == 'y'):
+        print("=========PROGRAM FOR UNIFICATION=========")
+        no_of_pred=int(input("Enter Number of Predicates:"))
+        for i in range(no_of_pred):
+        #nouse=input() #   //to accept "enter" as a character
+            print("Enter Predicate ",(i+1)," :")
+            predicate[i]=input()
+            print("Enter No.of Arguments for Predicate ",predicate[i]," :")
+            no_of_arg[i]=int(input())
 
-def is_variable(x):
-    "A variable is an Expr with no args and a lowercase symbol as the op."
-    return isinstance(x, Expr) and not x.args and is_var_symbol(x.op)
+            for j in range(no_of_arg[i]):
+                print("Enter argument ",j+1," :");
+                argument[i][j]=input()
 
-def unify_var(var, x, s):
-    if var in s:
-        return unify(s[var], x, s)
-    elif occur_check(var, x, s):
-        return None
-    else:
-        return extend(s, var, x)
+ 
+        display()
+        chk_arg_pred()
+        ch=input("Do you want to continue(y/n):   ")
 
-def occur_check(var, x, s):
-    """Return true if variable var occurs anywhere in x
-    (or in subst(s, x), if s has a binding for x)."""
-    if var == x:
-        return True
-    elif is_variable(x) and x in s:
-        return occur_check(var, s[x], s)
-    elif isinstance(x, Expr):
-        return (occur_check(var, x.op, s) or
-                occur_check(var, x.args, s))
-    elif isinstance(x, (list, tuple)):
-        return some(lambda element: occur_check(var, element, s), x)
-    else:
-        return False
+def display():
 
-def extend(s, var, val):
-    """Copy the substitution s and extend it by setting var to val;
-    return copy.
-    >>> ppsubst(extend({x: 1}, y, 2))
-    {x: 1, y: 2}
-    """
-    s2 = s.copy()
-    s2[var] = val
-    return s2
+    print("=======PREDICATES ARE======")
+    for i in range(no_of_pred):
+        print(predicate[i],"(", end="")
+        for j in range(no_of_arg[i]):
+            print(argument[i][j], end="")
+            if(j!=no_of_arg[i]-1):
+                print(",", end="")
+        print(")")
 
-def subst(s, x):
-    """Substitute the substitution s into the expression x.
-    >>> subst({x: 42, y:0}, F(x) + y)
-    (F(42) + 0)
-    """
-    if isinstance(x, list):
-        return [subst(s, xi) for xi in x]
-    elif isinstance(x, tuple):
-        return tuple([subst(s, xi) for xi in x])
-    elif not isinstance(x, Expr):
-        return x
-    elif is_var_symbol(x.op):
-        return s.get(x, x)
-    else:
-        return Expr(x.op, *[subst(s, arg) for arg in x.args])
 
-def fol_fc_ask(KB, alpha):
-    """Inefficient forward chaining for first-order logic. [Fig. 9.3]
-    KB is a FolKB and alpha must be an atomic sentence."""
-    while True:
-        new = {}
-        for r in KB.clauses:
-            ps, q = parse_definite_clause(standardize_variables(r))
-            raise NotImplementedError
+#/*==========UNIFY FUNCTION=========*/
 
-def standardize_variables(sentence, dic=None):
-    """Replace all the variables in sentence with new variables.
-    >>> e = expr('F(a, b, c) & G(c, A, 23)')
-    >>> len(variables(standardize_variables(e)))
-    3
-    >>> variables(e).intersection(variables(standardize_variables(e)))
-    set([])
-    >>> is_variable(standardize_variables(expr('x')))
-    True
-    """
-    if dic is None: dic = {}
-    if not isinstance(sentence, Expr):
-        return sentence
-    elif is_var_symbol(sentence.op):
-        if sentence in dic:
-            return dic[sentence]
-        else:
-            v = Expr('v_%d' % standardize_variables.counter.next())
-            dic[sentence] = v
-            return v
-    else:
-        return Expr(sentence.op,
-                    *[standardize_variables(a, dic) for a in sentence.args])
+def unify():
+    flag=0
+    for i in range(no_of_pred-1):
+        for j in range(no_of_arg[i]):
+            if(argument[i][j]!=argument[i+1][j]):
+                if(flag==0):
+                    print("======SUBSTITUTION IS======")
+                    print(argument[i+1][j],"/",argument[i][j])
+                    flag+=1
 
-standardize_variables.counter = itertools.count()
+        if(flag==0):
+            print("Arguments are Identical...")
+            print("No need of Substitution")
 
-a = input("Enter first exp: ")
-b = input("Enter second exp: ")
-s = input("Enter substitution: ")
 
-res = unify(a,b,c)
+def chk_arg_pred():
+    pred_flag=0
+    arg_flag=0
 
-print(res)
+
+   #/*======Checking Prediactes========*/
+    for i in range(no_of_pred-1):
+        if(predicate[i]!=predicate[i+1]):
+            print("Predicates not same..")
+            print("Unification cannot progress!")
+            pred_flag=1
+            break
+
+   #/*=====Chking No of Arguments====*/
+
+    if(pred_flag!=1):
+        ind=0
+        key = no_of_arg[ind]
+        l = len(no_of_arg)
+        for i in range(0,key-1):
+            if i>=key:
+                continue
+            if ind!=l-1:
+                ind += 1
+                key = no_of_arg[ind]
+            if(no_of_arg[i]!=no_of_arg[i+1]):
+
+                print("Arguments Not Same..!")
+                arg_flag=1
+                break
+
+        if(arg_flag==0 and pred_flag!=1):
+            unify()
+
+main()
+
+ 
